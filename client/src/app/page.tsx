c329990bb918bbf007b5558542d9151f2832fb31
Ravi Jaiswal <ravijaiswal258852@gmail.com>
2025-02-24 22:47:59 +0530

    feat: add meteor animation component and shiny button with enhanced styles

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,19 +1,27 @@
 "use client";
-import NavBar from "@/components/Header/NavBar";
-import { useAuth } from "@/context/AuthContext";
+import { GlobeDemo } from "@/components/Globe";
+import { SparklesCore } from "@/components/ui/sparkles";
 import React from "react";
 
 const Page = () => {
-  const { user, logout } = useAuth();
   return (
-    <div>
-      <div>
-        <NavBar />
+    <div className="w-full h-screen bg-black absolute">
+      {/* Sparkles in the background */}
+      <div className="fixed top-0 left-0 w-full h-full z-0">
+        <SparklesCore
+          background="transparent"
+          minSize={0.4}
+          maxSize={1}
+          particleDensity={80}
+          className="w-full h-full"
+          particleColor="#FFFFFF"
+        />
       </div>
-      {user?.email}
-      <p>{user?.id}</p>
 
-      <button onClick={logout}>logout</button>
+      {/* Globe in the foreground */}
+      <div className="relative w-full h-full flex items-center justify-center z-10">
+        <GlobeDemo />
+      </div>
     </div>
   );
 };
